03fc39a91e6d3ef8295db75a76486e8f98a7b852
Duong Minh Anh Khoi <duongminhanhkhoi14092002@gmail.com>
2025-12-10 06:17:30 +0700

    [REFACTOR]: refactor all plugin

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,6 +1,7 @@
 local keymaps = vim.keymap
 
-keymaps.set("n", "gl", "<cmd>bnext<CR>", { desc = "Next buffer" })
+keymaps.set("n", "bl", "<cmd>bnext<CR>", { desc = "Next buffer" })
+keymaps.set("n", "bj", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
 
 keymaps.set("n", "gl", function()
 	vim.diagnostic.open_float()
@@ -19,3 +20,6 @@ keymaps.set("n", "<leader>wh", "<C-w>h", { desc = "Go to the left window" })
 keymaps.set("n", "<leader>wl", "<C-w>h", { desc = "Go to the right window" })
 keymaps.set("n", "<leader>wj", "<C-w>j", { desc = "Go to the down window" })
 keymaps.set("n", "<leader>wk", "<C-w>k", { desc = "Go to the up window" })
+
+keymaps.set("n", "<leader>e", ":Neotree toggle position=left<CR>", { desc = "Toggle NeoTree" })
+keymaps.set("n", "<Leader>fo", "<cmd>Neotree reveal<cr>", { desc = "Reveal file" })
